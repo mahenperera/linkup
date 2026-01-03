d7b56d75d355219f9eb70b65522c29240f12a64b
Mahen Perera <kmaperera864@gmail.com>
2026-01-03 17:47:45 +0530

    Implement member profile update functionality.

--- API/Data/MemberRepository.cs
+++ API/Data/MemberRepository.cs
@@ -9,7 +9,7 @@ namespace API.Data
         public async Task<Member?> GetMemberByIdAsync(string id)
         {
             return await context.Members.FindAsync(id);
-        }
+        }   
 
         public async Task<IReadOnlyList<Member>> GetMembersAsync()
         {
@@ -32,6 +32,13 @@ namespace API.Data
         public void Update(Member member)
         {
             context.Entry(member).State = EntityState.Modified;
+        }
+
+        public async Task<Member?> GetMemberForUpdate(string id)
+        {
+            return await context.Members
+                .Include(x => x.User)
+                .SingleOrDefaultAsync(x => x.Id == id);
         }
     }
 }
--- API/Interfaces/IMemberRepository.cs
+++ API/Interfaces/IMemberRepository.cs
@@ -4,10 +4,11 @@ namespace API.Interfaces
 {
     public interface IMemberRepository
     {
-        void Update(Member member);
-        Task<bool> SaveAllAsync();
-        Task<IReadOnlyList<Member>> GetMembersAsync();
         Task<Member?> GetMemberByIdAsync(string id);
+        Task<IReadOnlyList<Member>> GetMembersAsync();
         Task<IReadOnlyList<Photo>> GetPhotosForMembersAsync(string memberId);
+        Task<bool> SaveAllAsync();
+        void Update(Member member);
+        Task<Member?> GetMemberForUpdate(string id);
     }
 }
