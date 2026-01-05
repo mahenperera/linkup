ab60fca64796943e9b0165e0a8ec835698ea3177
Mahen Perera <kmaperera864@gmail.com>
2026-01-05 22:45:52 +0530

    Implement photo deletion UI functionality.

--- frontend/src/features/members/member-photos/member-photos.ts
+++ frontend/src/features/members/member-photos/member-photos.ts
@@ -6,16 +6,17 @@ import { ImageUpload } from '../../../shared/image-upload/image-upload';
 import { AccountService } from '../../../core/services/account-service';
 import { User } from '../../../types/user';
 import { StarButton } from "../../../shared/star-button/star-button";
+import { DeleteButton } from "../../../shared/delete-button/delete-button";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload, StarButton],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
@@ -59,5 +60,13 @@ export class MemberPhotos implements OnInit {
         );
       },
     });
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update(photos => photos.filter(x => x.id !== photoId));
+      }
+    })
   }
 }
