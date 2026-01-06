0af41b76d6802ab996ffd68c28a29c858902039f
Mahen Perera <kmaperera864@gmail.com>
2026-01-06 16:06:45 +0530

    Implement registration form with profile details and validation.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,14 +5,26 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        public string DisplayName { get; set; } = "";
+        public string DisplayName { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
-        public string Email { get; set; } = "";
+        public string Email { get; set; } = string.Empty;
 
         [Required]
         [MinLength(4)]
-        public string Password { get; set; } = "";
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        public string Country { get; set; } = string.Empty;
+
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
     }
 }
