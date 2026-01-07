63921d4cc29fb9f619531d5a99cc000356536acf
Mahen Perera <kmaperera864@gmail.com>
2026-01-07 15:14:40 +0530

    Implement pagination and filtering for member list.

--- API/Interfaces/IMemberRepository.cs
+++ API/Interfaces/IMemberRepository.cs
@@ -1,11 +1,12 @@
 ﻿using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces
 {
     public interface IMemberRepository
     {
         Task<Member?> GetMemberByIdAsync(string id);
-        Task<IReadOnlyList<Member>> GetMembersAsync();
+        Task<PaginatedResult<Member>> GetMembersAsync(MemberParams memberParams);
         Task<IReadOnlyList<Photo>> GetPhotosForMembersAsync(string memberId);
         Task<bool> SaveAllAsync();
         void Update(Member member);
